ff671c34318d3c861a43b1f9932d5b2fad5b4a23
[username] <yanxuegan0903@163.com>
2018-01-16 16:46:50 +0800

    数据绑定

--- Demo/pages/posts/post.js
+++ Demo/pages/posts/post.js
@@ -1,3 +1,77 @@
 Page({
 
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var post_content1 = {
+      post_date: 'Sep 25 2016',
+      post_title: '正是虾肥蟹壮时',
+      post_content: '“菊黄蟹正肥，品尝秋之味”，徐志摩把“看初花的荻卢”和“到楼外楼吃蟹”，并列为秋天来杭州不能错过的风雅之事，用林妹妹的话讲是“蟹封嫩于双双满”。',
+      post_like:'92',
+      post_read:'68',
+      img_condition:true,
+      post_img: '/images/fan_1.jpeg',
+      post_author_img: '/images/user-icon.png'
+    }
+    
+    this.setData(post_content1);
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
 })
