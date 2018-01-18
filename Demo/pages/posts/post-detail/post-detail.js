80671e9668d0baa90f13595da568c7eef244e2f2
[username] <yanxuegan0903@163.com>
2018-01-18 14:27:47 +0800

    收藏文章完成

--- Demo/pages/posts/post-detail/post-detail.js
+++ Demo/pages/posts/post-detail/post-detail.js
@@ -15,13 +15,47 @@ Page({
    */
   onLoad: function (options) {
     var postid = options.id;
+    //  将postID 缓存早数据data中
+    this.data.currentPostid = postid;
     console.log('postid = ' + postid);
 
     var postData = postsData.postList[postid];
+    // 设置数据
     this.setData({
-      postData:postData,
+      postData: postData,
     });
 
+    //  获取这篇文章是否已经收藏
+    var postsCollected = wx.getStorageSync('posts_Collected');
+    if(postsCollected){
+      console.log("postsCollected = "+postsCollected);
+      var postCollected = postsCollected[postid];
+      console.log("是否收藏 = " + postCollected);
+      this.setData({
+        collected: postCollected
+      })
+    }else{
+      console.log("postsCollected 不存在");
+      var postsCollected = {};
+      postsCollected[postid] = false;
+      wx.setStorageSync("posts_Collected", postsCollected);
+    }
+
+
   },
+
+  onCollectedTap:function(event){
+  
+    var postsCollected = wx.getStorageSync('posts_Collected')
+    var postCollected = postsCollected[this.data.currentPostid]
+    postCollected = !postCollected
+    postsCollected[this.data.currentPostid] = postCollected;
+    wx.setStorageSync('posts_Collected', postsCollected)
+    
+    this.setData({
+      collected: postCollected
+    });
+
+  }
 
 })
